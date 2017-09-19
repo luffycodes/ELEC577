a5d96c017cce46a0c980e7f0d6689846e20cf5b2
luffycodes <sonkar.iitk@gmail.com>
2017-09-19 18:49:58 -0500

    1. Code not generalizable 2. Heavily catered to assignment 1

--- nesterov_gradient.py
+++ nesterov_gradient.py
@@ -4,28 +4,45 @@
 import matplotlib.pyplot as plt
 from numpy import *
 
-# generate a random problem instance 
-n = 5
-A = random.randn(n, n)
-Q = dot(A.T, A)
-b = random.randn(n)
-
-xopt = dot(linalg.inv(Q), b)
-
-eigenvalues = linalg.eigvals(Q)
-M = max(eigenvalues)
-m = min(eigenvalues)
-
-# gradient descent
-
 niter = 100
-stepsize = 2 / (M + m)
+stepsize = 1 / 8
 distance = []
-xk = zeros(n)
-# random.randn(n) # random initializer
-for k in range(niter):
-    xk = xk - stepsize * (dot(Q, xk) - b)
-    distance.append(linalg.norm(xk - xopt))
+xopt = 0
+best_alpha = 1
+best_beta = 1
+best_closeness = 1
 
-plt.plot(log(distance))
-plt.show()
+
+def getQ_BasedOnX(xk1):
+    if xk1 > 0 or xk1 < -3:
+        return 16
+    else:
+        return 1
+
+
+def getB_BasedOnX(xk1):
+    if xk1 < -3:
+        return 45
+    else:
+        return 0
+
+
+def get_gradient(xk1):
+    return getQ_BasedOnX(xk1) + getB_BasedOnX(xk1)
+
+
+for alpha in arange(0.0, 1.0, 0.01):
+    for beta in arange(0.0, 1.0, 0.01):
+        xk = 2
+        xk_last = 2
+        for k in range(niter):
+            diff_last_two = beta * (xk - xk_last)
+            xk_last = xk
+            xk = xk - alpha * get_gradient(xk + diff_last_two) + diff_last_two
+            distance = log(linalg.norm(xk - xopt))
+            if distance < best_closeness:
+                best_closeness = distance
+                best_alpha = alpha
+                best_beta = beta
+
+print(best_closeness, best_beta, best_alpha)
