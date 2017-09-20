175809ad321aa75ea44367e55604740f512df306
luffycodes <sonkar.iitk@gmail.com>
2017-09-19 19:25:21 -0500

    1. Looks like the code is buggy

--- nesterov_gradient.py
+++ nesterov_gradient.py
@@ -4,9 +4,9 @@
 import matplotlib.pyplot as plt
 from numpy import *
 
-niter = 100
+niter = 10
 stepsize = 1 / 8
-distance = []
+plot_distance = []
 xopt = 0
 best_alpha = 1
 best_beta = 1
@@ -31,18 +31,29 @@ def get_gradient(xk1):
     return getQ_BasedOnX(xk1) + getB_BasedOnX(xk1)
 
 
+def compute_convergence(alpha1, beta1, plot=False):
+    global best_closeness, best_alpha, best_beta, plot_distance
+    xk = 2
+    xk_last = 2
+    for k in range(niter):
+        diff_last_two = beta1 * (xk - xk_last)
+        xk_last = xk
+        xk = xk - alpha1 * get_gradient(xk + diff_last_two) + diff_last_two
+        distance = linalg.norm(xk - xopt)
+        if plot:
+            plot_distance.append(distance)
+        if distance < best_closeness:
+            best_closeness = distance
+            best_alpha = alpha1
+            best_beta = beta1
+
+
 for alpha in arange(0.0, 1.0, 0.01):
     for beta in arange(0.0, 1.0, 0.01):
-        xk = 2
-        xk_last = 2
-        for k in range(niter):
-            diff_last_two = beta * (xk - xk_last)
-            xk_last = xk
-            xk = xk - alpha * get_gradient(xk + diff_last_two) + diff_last_two
-            distance = log(linalg.norm(xk - xopt))
-            if distance < best_closeness:
-                best_closeness = distance
-                best_alpha = alpha
-                best_beta = beta
+        compute_convergence(alpha, beta)
 
-print(best_closeness, best_beta, best_alpha)
+print(best_closeness, best_alpha, best_beta)
+
+compute_convergence(1/16, 0, True)
+plt.plot(plot_distance)
+plt.show()
