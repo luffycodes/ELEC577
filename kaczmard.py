30d417d89f4d6e806d8848dd199485f0c9724d31
luffycodes <sonkar.iitk@gmail.com>
2017-10-04 13:47:30 -0500

    1. Bug fix - did not normalize A

--- kaczmard.py
+++ kaczmard.py
@@ -1,24 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import normalize
 
 n = 500
 d = 50
 A = np.random.randn(n, d)
+A = normalize(A, axis=1, norm='l2')
+
 x_optimal = np.random.randn(d)
 b = np.dot(A, x_optimal)
-stepsize = 1
+for stepsize in np.arange(0.2, 2, 0.4):
+    niter = 1000
+    distance = []
+    xk = np.random.randn(d)
+    for k in range(niter):
+        randomA = A[np.random.randint(0, n)]
+        randomA = randomA.reshape(d, 1)
 
-niter = 1000
-distance = []
-xk = np.zeros(d)
-for k in range(niter):
-    randomA = A[np.random.randint(0, n)]
-    randomA.shape = (d, 1)
-    lhs = np.identity(d) - stepsize * np.dot(randomA, randomA.transpose())
-    rhs = xk - x_optimal
-    xk = np.dot(lhs, rhs) + stepsize * x_optimal
-    distance.append(np.linalg.norm(xk - x_optimal))
-    print(np.linalg.norm(xk - x_optimal))
+        lhs = np.identity(d) - stepsize * np.dot(randomA, randomA.transpose())
+        rhs = xk - x_optimal
+        xk = np.dot(lhs, rhs) + x_optimal
+        distance.append(np.linalg.norm(xk - x_optimal))
+        print(np.linalg.norm(xk - x_optimal))
 
-plt.plot(np.log(distance))
+    plt.plot(np.log(distance), label=("step:", "%.2f" % round(stepsize, 2)))
+
+plt.legend()
+plt.xlabel("iterations")
+plt.ylabel("error")
 plt.show()
