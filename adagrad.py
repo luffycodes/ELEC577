b0c422dae029ff44337fb2531d1c542f82cb1186
luffycodes <sonkar.iitk@gmail.com>
2017-10-31 23:57:18 -0500

    1. SGD working

--- adagrad.py
+++ adagrad.py
@@ -7,7 +7,7 @@ m = 5000
 n = 1000
 A = np.zeros((m, n))
 x_opt = np.zeros(n)
-x_rand = np.zeros(n)
+x_rand = np.ones(n)
 
 # Code to generate A - tune sparse to generate different A types
 for j in range(1, n + 1):
@@ -15,16 +15,16 @@ for j in range(1, n + 1):
         prob_sign = 1 / j
     else:
         prob_sign = 1
-    for iterCount in range(1, m + 1):
+    for i in range(1, m + 1):
         p = np.random.binomial(1, p=prob_sign)
         if p == 1:
             sign = np.random.binomial(1, p=0.5)
             if sign == 0:
-                A[iterCount - 1][j - 1] = 1
+                A[i - 1][j - 1] = 1
             else:
-                A[iterCount - 1][j - 1] = -1
+                A[i - 1][j - 1] = -1
 
-# Code to generate random X
+# Code to generate random X_opt
 for j in range(1, n + 1):
     p = np.random.binomial(1, p=0.5)
     if p == 1:
@@ -32,22 +32,47 @@ for j in range(1, n + 1):
     else:
         x_opt[j - 1] = -1
 
+# Code to generate random X
+for j in range(1, n + 1):
+    p = np.random.binomial(1, p=0.5)
+    if p == 1:
+        x_rand[j - 1] = 1
+    else:
+        x_rand[j - 1] = -1
+
 b = A.dot(x_opt)
 # Code to generate random B
-for iterCount in range(1, m + 1):
+for i in range(1, m + 1):
+    if b[i - 1] > 0:
+        b[i - 1] = 1
+    else:
+        b[i - 1] = -1
     p = np.random.binomial(1, p=0.95)
     if p == 0:
-        b[iterCount - 1] = - 1 * b[iterCount - 1]
+        b[i - 1] = - 1 * b[i - 1]
+
+fx = 0
+for i in range(1, m + 1):
+    fx = fx + np.maximum(0, 1 - b[i-1] * np.dot(A[i-1], x_opt))
+fx = fx/m
 
 # SGD
-alpha = 0.001
+alpha = 10
 distance = []
-for iterCount in range(1, 200000):
-    i = random.randint(0, m-1)
-    cond = 1 - b[i] * np.dot(A[i], x_rand)
+for i in range(1, 200000):
+    i_rand = random.randint(0, m - 1)
+    cond = 1 - b[i_rand] * np.dot(A[i_rand], x_rand)
     if cond > 1:
-        x_rand = x_rand + alpha/np.sqrt(iterCount + 1) * b[i] * A[i]
-    distance.append(np.linalg.norm(x_rand - x_opt))
+        x_rand = x_rand + alpha/np.sqrt(i + 1) * b[i_rand] * A[i_rand]
+
+    if i % 1000 == 0:
+        fx_k = 0
+        for j in range(1, m + 1):
+            fx_k = fx_k + np.maximum(0, 1 - b[j-1] * np.dot(A[j-1], x_rand))
+        fx_k = fx_k / m
+        norm = np.linalg.norm(fx - fx_k)
+        distance.append(norm)
+        print("step:", i, " & error:", norm)
 
 plt.plot(np.log(distance), label="SGD")
 plt.xlabel("iterations")
